536af533422a2d5c4ebf5f67cc63d5c5a0c40c95
Guillermo <guillhermoherrera@gmail.com>
2020-10-20 15:55:37 -0500

    show, edit, destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,33 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(titulo: params[:article][:titulo], contenido: params[:article][:contenido])
+    redirect_to @article
   end
 
   def create
     @article = Article.create(titulo: params[:article][:titulo], contenido: params[:article][:contenido])
     render json: @article
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+  
     
 end
