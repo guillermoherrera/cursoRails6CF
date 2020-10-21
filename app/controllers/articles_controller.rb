9a6287a5125e9f6c2042ff2093a403c019741b5a
Guillermo <guillhermoherrera@gmail.com>
2020-10-21 12:37:41 -0500

    before_action y parciales

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, except: [:new, :create, :index]
+
+  def index
+    @articles = Article.all
+  end
+  
 
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
   
   def new
@@ -9,11 +15,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.update(titulo: params[:article][:titulo], contenido: params[:article][:contenido])
     redirect_to @article
   end
@@ -24,9 +30,13 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
     redirect_to root_path
+  end
+  
+  def find_article
+    @article = Article.find(params[:id])
   end
   
     
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/bienvenido', to: 'home#index'
 
-  get 'articles/new', to: 'articles#new'
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: :new_articles
   get 'articles/:id', to: 'articles#show'
   get 'articles/:id/edit', to: 'articles#edit'
   
