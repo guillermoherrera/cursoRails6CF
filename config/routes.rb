20953f8efda5042339ad9038ba152e0749681d94
Guillermo <guillhermoherrera@gmail.com>
2020-10-23 08:44:27 -0500

    avatar del user
    has_one_attached :avatar

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   get '/bienvenido', to: 'home#index'
-
+  get '/perfil', to: 'users#edit' 
+  resources :users, only: [:update]
   #get 'articles/user/:user_id', to: 'articles#from_author'
 
   resources :articles do
